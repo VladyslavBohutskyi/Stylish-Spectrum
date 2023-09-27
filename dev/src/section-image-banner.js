4339a9a3f80c0d8fd50f1d0d6520856abd6e3de7
VladyslavBohutskyi <nonseinvenit@gmail.com>
2023-09-27 14:17:14 +0800

    add slider

--- dev/src/section-image-banner.js
+++ dev/src/section-image-banner.js
@@ -5,7 +5,22 @@ if (!customElements.get('image-banner')) {
     }
 
     connectedCallback() {
+      this.speed = (this.getAttribute('data-speed'))
+      this.settings = {
+        cellAlign: 'center',
+        contain: false,
+        wrapAround: true,
+        pageDots: true,
+        prevNextButtons: false
+      }
+      this.speed == 'false' ? false : this.settings.autoPlay = +this.speed;
 
+      this.initSlider();
     }
+
+    initSlider() {
+      this.slider = new Flickity(this.querySelector('.s-image-banner'), this.settings);
+    }
+
   })
 }
