bf592fe6fd7bd22668b3fec1bf5758dc8672318d
VladyslavBohutskyi <nonseinvenit@gmail.com>
2023-10-06 19:14:18 +0800

    mob menu  js scss liquid

--- dev/src/header.js
+++ dev/src/header.js
@@ -6,37 +6,82 @@ if (!customElements.get('s-header')) {
     }
 
     connectedCallback() {
-      this.mobMenu()
-      this.stickyHeader()
-      this.megaMenu()
+      this.megaMenus = Array.from(this.querySelectorAll('.s-mega-menu'))
+      this.menuItems = this.querySelectorAll('.s-header__menu_item')
+      this.menuItems.forEach((menuItem) => {
+        this.addClassParentMenu(menuItem)
+        this.openMegaMenuMobile(menuItem)
+        this.openMegaMenuDesktop(menuItem)
+        this.closeMegaMenuDesktop(menuItem)
+      })
+      this.toggleMenuMobile()
+      this.closeMegaMenuMobile()
     }
 
-    mobMenu() {
-
+    addClassParentMenu(menuItem) {
+      this.megaMenus.map((megaMenuItem) => {
+        menuItem.getElementsByTagName('a')[0].innerHTML == megaMenuItem.getAttribute('data-parent-menu')
+          ? menuItem.classList.add('s-header__menu_item--parent')
+          : false
+      })
     }
-    stickyHeader() {
 
-    }
-
-    megaMenu() {
-      const megaM = Array.from(this.querySelectorAll('.s-mega-menu'))
-      this.querySelectorAll('.s-header__menu_item').forEach((menuItem) => {
-
-        megaM.map((megaItem) => {
-          menuItem.getElementsByTagName('a')[0].innerHTML == megaItem.getAttribute('data-parent-menu') ? menuItem.classList.add('s-header__menu_item--parent') : false
-        })
-        menuItem.addEventListener('mouseover', (e) => {
-          megaM.map((megaItem) => {
-            e.target.innerHTML == megaItem.getAttribute('data-parent-menu') ? megaItem.classList.add('active') : false
-          })
-        })
-        menuItem.addEventListener('mouseout', (e) => {
-          megaM.map((megaItem) => {
-            e.target.innerHTML == megaItem.getAttribute('data-parent-menu') ? megaItem.classList.remove('active') : false
-          })
+    openMegaMenuDesktop(menuItem) {
+      menuItem.addEventListener('mouseover', (e) => {
+        this.megaMenus.map((megaMenuItem) => {
+          e.target.innerHTML == megaMenuItem.getAttribute('data-parent-menu')
+            ? megaMenuItem.classList.add('active')
+            : false
         })
       })
     }
-   }
+
+    closeMegaMenuDesktop(menuItem) {
+      menuItem.addEventListener('mouseout', (e) => {
+        this.megaMenus.map((megaMenuItem) => {
+          e.target.innerHTML == megaMenuItem.getAttribute('data-parent-menu')
+            ? megaMenuItem.classList.remove('active')
+            : false
+        })
+      })
+    }
+
+    toggleMenuMobile() {
+      this.querySelector('.s-header__menu_btn').addEventListener(
+        "click", () => (
+          this.querySelector('.s-header').classList.toggle('s-header--menu-active'),
+          document.querySelector('body').classList.toggle('hidden'),
+          this.querySelector('.s-header__menu').classList.remove('hide'),
+          this.megaMenus.map((e) => {
+            e.classList.remove('active')
+          })
+        )
+      )
+    }
+
+    openMegaMenuMobile(menuItem) {
+      menuItem.querySelector('.s-header__menu_item_arrow').addEventListener('click', (e) => {
+        this.megaMenus.map((megaMenuItem) => {
+          if (e.target.getAttribute('data-menu-name') == megaMenuItem.getAttribute('data-parent-menu')) {
+            megaMenuItem.classList.add('active')
+            this.querySelector('.s-header__menu').classList.add('hide')
+          }
+        })
+      })
+    }
+
+    closeMegaMenuMobile() {
+      this.querySelectorAll('.s-mega-menu__close').forEach((closeMegaMenu) => {
+        closeMegaMenu.addEventListener('click', (e) => {
+          this.querySelector('.s-header__menu').classList.remove('hide')
+          this.megaMenus.map((e) => {
+            e.classList.remove('active')
+          })
+        }
+        )
+      })
+    }
+
+  }
   )
 }
