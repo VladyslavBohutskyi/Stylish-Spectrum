2bbb1cd9b331f0434389594df9a16a1ea595c2a6
VladyslavBohutskyi <nonseinvenit@gmail.com>
2023-10-10 13:36:24 +0800

    select function

--- dev/src/main-product.js
+++ dev/src/main-product.js
@@ -6,9 +6,29 @@ if (!customElements.get('main-product')) {
     }
 
     connectedCallback() {
-
+      // this.variantsScript = JSON.parse(this.querySelector('[type="product_variant"]').textContent) 
+      this.variants = JSON.parse(this.getAttribute('data-variants'))
+      this.options = this.querySelectorAll('.variants-option')
+      this.options.forEach((option) => (
+        option.addEventListener('change', (e) =>  this.variantOnClick())
+      ))
     }
 
+
+    variantOnClick(){
+      let tempVariant = ''
+      this.options.forEach( (variant) => {
+        tempVariant += `${variant.querySelector('input:checked').value} / `
+      })
+      tempVariant = tempVariant.slice(0, -3)
+      let currentVariantId = ''
+      this.variants.map((variant) => {
+        variant.title == tempVariant 
+        ? currentVariantId = variant.id
+        : false
+      })
+      this.querySelector('#variant-select').value = currentVariantId
+    }
   }
  )
 }
