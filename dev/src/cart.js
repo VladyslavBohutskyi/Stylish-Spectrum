eb5802644821d3b31b9a160b63a476e317f7dcb8
VladyslavBohutskyi <nonseinvenit@gmail.com>
2023-10-22 16:21:04 +0800

    plus minus dell cart

--- dev/src/cart.js
+++ dev/src/cart.js
@@ -10,6 +10,42 @@ if (!customElements.get('cart-main')) {
 
     initCart() {
 
+      this.querySelectorAll('.change_qty').forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+          this.changeCount(e.target.dataset.variant_id, e.target.dataset.quantity)
+        })
+      })
+
     }
+
+    changeCount(id, quantity) {
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          'id': id,
+          'quantity': quantity
+        })
+      }
+
+      fetch(window.theme.shopUrl + '/cart/change.js', requestOptions)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          } else {
+            return response.json()
+          }
+
+        })
+        .then((data) => {
+          console.log(data)
+        })
+
+    }
+
+
+
   })
 }
