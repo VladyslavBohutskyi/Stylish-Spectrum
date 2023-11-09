7ab7ba16420939ebef46a8a664c06ac732b6d555
VladyslavBohutskyi <nonseinvenit@gmail.com>
2023-11-09 12:27:50 +0800

    finish

--- dev/src/section-review-slider.js
+++ dev/src/section-review-slider.js
@@ -6,9 +6,29 @@ if (!customElements.get('s-review-slider')) {
     }
 
     connectedCallback() {
-      
+      this.initSlider()
+      this.getAttribute('data-slider_apply') == 'true'
+        ? this.querySelectorAll('.s-review-slider__review').forEach((e) => e.classList.add('full-height'))
+        : false
     }
 
-  }
-  )
+    initSlider() {
+      this.speed = this.getAttribute('data-speed')
+      this.apply = (this.getAttribute('data-slider_apply') == 'true' ? false : true)
+
+      this.settings = {
+        prevNextButtons: false,
+        pageDots: false,
+        cellAlign: 'left',
+        contain: true,
+        draggable: true,
+        watchCSS: this.apply
+      }
+
+      this.speed == 'false' ? false : this.settings.autoPlay = +this.speed;
+
+      this.slider = new Flickity(this.querySelector('.s-review-slider__slider'), this.settings)
+    }
+  })
+
 }
