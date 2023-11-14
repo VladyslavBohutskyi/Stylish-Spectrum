29244fbca463841b76ce478fe0d39a406f28c642
VladyslavBohutskyi <nonseinvenit@gmail.com>
2023-11-14 15:49:38 +0800

    update collection

--- dev/src/section-best-sellers.js
+++ dev/src/section-best-sellers.js
@@ -3,20 +3,53 @@ if (!customElements.get('best-sellers')) {
     constructor() {
       super();
     }
+    nameCollection = this.dataset.collection
+    numberPage = 1
+    countLoadMore = 1
+    countProducts = this.dataset.count
+    collectionProducts = []
+    countCollectionProducts = null
+
 
     connectedCallback() {
       this.querySelector('.best-sellers__btn').addEventListener('click', () => this.loadMore())
+      this.updateCollection()
     }
 
-    loadMore(){
-      fetch(window.theme.shopUrl + `/collections/best-sellers?page=1`)
-        .then((res) => res.text())
-        .then((data) => {
-          const html = new DOMParser().parseFromString(data, "text/html");
-          html.querySelectorAll('.main-collection__product').forEach(e => e.classList.value = 'best-sellers__product')
-          this.querySelector('.best-sellers__products').innerHTML = html.querySelector('.main-collection__products').innerHTML 
-          console.log(html)
-        })
+    updateCollection() {
+      if (this.countCollectionProducts != false) {
+        fetch(window.theme.shopUrl + `/collections/${this.nameCollection}?page=${this.numberPage}`)
+          .then((res) => res.text())
+          .then((data) => {
+            const html = new DOMParser().parseFromString(data, "text/html");
+            html.querySelectorAll('.main-collection__product').forEach(el => el.classList.value = 'best-sellers__product')
+
+            const products = Array.from(html.querySelectorAll('.best-sellers__product'))
+
+            this.countCollectionProducts = this.countCollectionProducts ?? products.length
+
+            if (this.countCollectionProducts > products.length) {this.countCollectionProducts = false}
+
+            this.collectionProducts = [...this.collectionProducts, ...products]
+            this.numberPage++
+
+            if (this.collectionProducts.length < (this.countProducts * this.countLoadMore + (+this.countProducts))) {
+              this.updateCollection()
+            }
+          })
+      }
+    }
+
+    loadMore() {
+      this.collectionProducts.forEach((prod, index) => {
+        console.log(prod);
+        console.log(index);
+        if(index > this.countProducts * this.countLoadMore && index < (this.countProducts * this.countLoadMore + (+this.countProducts))){
+          this.querySelector('.best-sellers__products').appendChild(prod)
+        }
+      })
+
+      
     }
   })
 }
