5c5fceed30719a38ba272310c7876ac4732a413f
VladyslavBohutskyi <nonseinvenit@gmail.com>
2023-12-05 16:50:33 +0800

    popup-search - layout and js fix

--- dev/src/section-popup-search.js
+++ dev/src/section-popup-search.js
@@ -1,47 +1,61 @@
 if (!customElements.get('popup-search')) {
   customElements.define('popup-search', class PopupSearch extends HTMLElement {
-      constructor() {
-        super();
-      }
+    constructor() {
+      super();
+    }
 
-      searchInput = this.querySelector('.popup-search__input')
-      connectedCallback() {
-        PubSub.subscribe('open-search', this.openSearch.bind(this))
-        this.querySelector('.icon-close').addEventListener('click', this.closeSearch.bind(this))
-        this.searchInput.addEventListener('input', () => this.setValue(this.searchInput.value))
-        this.querySelector('.popup-search__btn').addEventListener('click', () => this.search())
-      }
+    searchInput = this.querySelector('.popup-search__input')
 
-      openSearch(){
-        this.classList.add('active')
-      }
+    connectedCallback() {
+      PubSub.subscribe('open-search', this.openSearch.bind(this))
+      this.querySelector('.icon-close').addEventListener('click', this.closeSearch.bind(this))
+      this.searchInput.addEventListener('input', () => this.setValue(this.searchInput.value))
+      this.querySelector('.popup-search__btn').addEventListener('click', () => this.search())
+      window.addEventListener('keydown', this.searchEnter.bind(this))
+    }
 
-      closeSearch(){
-        this.classList.remove('active')
-      }
 
-      setValue(value){
-       this.querySelector('.form-hidden-input').value = value
+    searchEnter(e) {
+      if (this.classList.contains('active') && e.keyCode === 13) {
+        this.search();
       }
+    }
 
-      search(){
-        const form = this.querySelector('.popup-search__form-hidden')
-        const formUrl = window.theme.shopUrl + window.theme.routes.search_page
-        const formBody = {
-          method: 'POST',
-          body: new FormData(form)
-        }
-        fetch(formUrl, formBody)
-          .then((res) => {
+    openSearch() {
+      this.classList.add('active')
+    }
+
+    closeSearch() {
+      this.classList.remove('active')
+    }
+
+    setValue(value) {
+      this.querySelector('.form-hidden-input').value = value
+    }
+
+    search() {
+      const form = this.querySelector('.popup-search__form-hidden')
+      const formUrl = window.theme.shopUrl + window.theme.routes.search_page
+      const formBody = {
+        method: 'POST',
+        body: new FormData(form)
+      }
+      fetch(formUrl, formBody)
+        .then((res) => {
           if (res.ok) {
             return res.text()
           }
         })
-          .then((data) => {
-            const html = new DOMParser().parseFromString(data, "text/html");
-            this.querySelector('.popup-search__results').innerHTML = html.querySelector('.search-main__results').innerHTML
-          });
-      }
+        .then((data) => {
+          const html = new DOMParser().parseFromString(data, "text/html");
+          const searchResultContainer = this.querySelector('.popup-search__results')
+          if (html.querySelector('.search-main__results_col') !== null) {
+            searchResultContainer.innerHTML = html.querySelector('.search-main__results').innerHTML
+          } else {
+            searchResultContainer.innerHTML = 'No matches found'
+          }        
+        });
     }
+  }
   )
 }
